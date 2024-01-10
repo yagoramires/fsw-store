fa9ad2535456afe2d761ec8efcaa0d9a8ed1b89e
yagoramires <yago.ramiresx@gmail.com>
2024-01-10 11:02:39 -0300

    chore: add domain to next js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fsw-store.s3.sa-east-1.amazonaws.com",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
